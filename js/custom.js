06ec47a41225c6e6f4d370ac20a25c564b8fc4f8
TiagoSemedoVaz <37613@esjaloures.org>
2026-03-05 10:23:00 +0000

    siteeeee

--- js/custom.js
+++ js/custom.js
@@ -1,45 +1,34 @@
-
-  (function ($) {
-  
+(function ($) {
+ 
   "use strict";
-
+ 
     // NAVBAR
     $('.navbar-collapse a').on('click',function(){
       $(".navbar-collapse").collapse('hide');
     });
-
-    $(function() {
-      $('.hero-slides').vegas({
-          slides: [
-  { src: 'https://via.placeholder.com/400x400' },
-  { src: 'https://via.placeholder.com/400x400' },
-  { src: 'https://via.placeholder.com/400x400' }
-]
-          timer: false,
-          animation: 'kenburns',
-      });
-    });
-    
+ 
+  
     // CUSTOM LINK
     $('.smoothscroll').click(function(){
       var el = $(this).attr('href');
       var elWrapped = $(el);
       var header_height = $('.navbar').height() + 60;
-  
+ 
       scrollToDiv(elWrapped,header_height);
       return false;
-  
+ 
       function scrollToDiv(element,navheight){
         var offset = element.offset();
         var offsetTop = offset.top;
         var totalScroll = offsetTop-navheight;
-  
+ 
         $('body,html').animate({
         scrollTop: totalScroll
         }, 300);
       }
     });
-  
+ 
   })(window.jQuery);
-
-
+ 
+ 
+ 
